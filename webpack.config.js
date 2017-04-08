cf5e804064025f683d391c149efdbc070d3d13dc
HelloWorld017 <deu07115@gmail.com>
2017-04-09 00:03:09 +0900

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const cssLoaders = [
+	{
+		loader: 'css-loader',
+		options: {
+			importLoaders: 1
+		}
+	},
+	'postcss-loader'
+];
+
+const cssExtract = ExtractTextPlugin.extract({
+	use: cssLoaders,
+	fallback: 'vue-style-loader'
+});
+
+const lessExtract = ExtractTextPlugin.extract({
+	use: cssLoaders.concat('less-loader'),
+	fallback: 'vue-style-loader'
+});
+
+module.exports = {
+	entry: path.resolve(__dirname, 'lib', 'index.js'),
+
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/dist/',
+		filename: 'qmxtr-volume.bundle.js',
+		library: 'VolumePlugin',
+		libraryTarget: 'umd'
+	},
+
+	module: {
+		rules: [
+			{
+				test:/\.vue$/,
+				loader: 'vue-loader',
+				options: {
+					loaders: {
+						'less': lessExtract,
+						'css': cssExtract,
+						'js': {
+							loader: 'babel-loader',
+							options: {
+								presets: ['env']
+							}
+						}
+					}
+				}
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['env']
+				},
+				exclude: /node_modules/
+			},
+			{
+				test: /\.less$/,
+				loader: lessExtract
+			},
+			{
+				test: /\.css$/,
+				loader: cssExtract
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|ttf|woff2?|eot)$/,
+				loader: 'file-loader',
+				options: {
+					name: 'files/[name].[ext]?[hash]'
+				}
+			}
+		]
+	},
+
+	plugins: [
+		new ExtractTextPlugin('qmxtr-equalizer.bundle.css')
+	],
+
+	resolve: {
+		alias: {
+			'variables': path.resolve(__dirname, 'app', 'less', 'variables.less'),
+			'theme': path.resolve(__dirname, 'app', 'less', 'theme.less')
+		}
+	},
+
+	devtool: '#eval-source-map'
+};
+
+if(process.env.NODE_ENV === 'production'){
+	module.exports.devtool = '#source-map';
+	module.exports.plugins = (module.exports.plugins || []).concat([
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"production"'
+			}
+		})
+	]);
+}
